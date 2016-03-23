ce5abf21365a010542f0d8415eafaf972e6baabf
Blake Scott and Haley Bristol <haley@>
2016-03-23 10:47:42 -0700

    add model and keg component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,29 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div *ngFor="#keg of kegs" class="container">
+      <h1>Bailey's Tap Room App</h1>
+      <h4>{{ keg.name }}</h4>
+      <h4>{{ keg.brand}}</h4>
+      <h4>{{ keg.price }}</h4>
+      <h4>{{ keg.alContent }}</h4>
+      <h4>{{ keg.pintsLeft }}</h4>
+    </div>
   `
 })
+export class KegComponent {
+    public keg: Keg;
+    constructor(){
+    this.keg = new Keg("The Abyss", "Deschutes", "5", "11.5", 0);
+  }
+}
 
-export class AppComponent {
 
+
+
+
+export class Keg {
+  public pintsLeft: number = 124;
+  constructor(public name: string, public brand: string, public price: string, public alContent: string, public id: number) {
+}
 }
