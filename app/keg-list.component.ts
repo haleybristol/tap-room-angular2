9c9d55349c1ea9cbd079f2ea87811b9ffc0116ab
Blake Scott and Haley Bristol <blake+haley@>
2016-03-23 14:56:22 -0700

    move KegListComponent to its own folder

---
+++ app/keg-list.component.ts
@@ -0,0 +1,25 @@
+//Child of the Root/////////////////////////////
+  @Component({
+    selector: 'keg-list',
+    inputs: ['kegList'],
+    outputs: ['onKegSelect'],
+    directives: [KegComponent],
+    template: `
+    <keg-display *ngFor="#currentKeg of kegList" (click)="kegClicked(currentKeg)" [class.selected]="currentKeg === selectedKeg" [keg]="currentKeg">
+    </keg-display>
+    `
+  })
+
+  export class KegListComponent {
+    public kegList: Keg[];
+    public onKegSelect: EventEmitter<Keg>;
+    public selectedKeg: Keg;
+    constructor() {
+      this.onKegSelect = new EventEmitter();
+    }
+    kegClicked(clickedKeg: Keg): void {
+      console.log(clickedKeg);
+      this.selectedKeg = clickedKeg;
+      this.onKegSelect.emit(clickedKeg);
+    }
+  }
