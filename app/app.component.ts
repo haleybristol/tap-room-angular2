a5fef8bc224faddf6b3ef3c41c81eb3fcd3a05e8
Blake Scott and Haley Bristol <blake+haley@>
2016-03-23 13:15:20 -0700

    add taskListComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,61 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
+//Child////////////////////////////////////
+
+  @Component({
+    selector: 'keg-list',
+    inputs: ['kegList'],
+    // outputs: ['onKegSelect'],
+    template: `
+    <div *ngFor="#currentKeg of kegList" (click)="kegClicked(currentKeg)" class="container">
+      <h4>{{ currentKeg.name }}</h4>
+      <h4>Brewery:{{ currentKeg.brand }}</h4>
+      <h4>$ {{ currentKeg.price }}</h4>
+      <h4>Alcohol% {{ currentKeg.alContent }}</h4>
+      <h4>Pints left: {{ currentKeg.pintsLeft }}</h4>
+    </div>
+    `
+  })
+
+  export class KegListComponent {
+    public kegList: Keg[];
+    kegClicked(clickedKeg: Keg): void {
+      console.log(clickedKeg);
+    }
+  }
+
+
+//Parent////////////////////////////////////
 @Component({
   selector: 'my-app',
+  directives: [KegListComponent],
   template: `
     <h1>Bailey's Tap Room App</h1>
-    <div *ngFor="#keg of kegs" (click)="kegWasSelected(keg)" class="container">
-      <h4>{{ keg.name }}</h4>
-      <h4>Brewery:{{ keg.brand}}</h4>
-      <h4>$ {{ keg.price }}</h4>
-      <h4>Alcohol% {{ keg.alContent }}</h4>
-      <h4>Pints left: {{ keg.pintsLeft }}</h4>
+    <div class="container">
+      <keg-list [kegList]="kegs"></keg-list>
     </div>
   `
 })
 export class AppComponent {
-    public kegs: Keg[];
-    constructor(){
+  public kegs: Keg[];
+  constructor(){
     this.kegs = [
     new Keg("The Abyss", "Deschutes", "5", "11.5", 0),
     new Keg("Mirror Pond", "Deschutes", "5", "6.5", 1),
     new Keg("Old Chub", "Oskar Meyer Blue's", "4", "6.5", 2),
     new Keg("Apocalypse", "10 Barrel", "5", "7.5", 3),
     new Keg("Citrus Mistress", "Hop Valley", "6", "7", 4)
-  ];
+    ];
   }
   kegWasSelected(clickedKeg: Keg): void {
     console.log(clickedKeg);
   }
 }
 
+//Model////////////////////////////////////
 
 export class Keg {
   public pintsLeft: number = 124;
   constructor(public name: string, public brand: string, public price: string, public alContent: string, public id: number) {
-}
+  }
 }
