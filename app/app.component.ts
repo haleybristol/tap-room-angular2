151f66d105ca82e16efd8ed1d8a82c89d39250c0
Blake Scott and Haley Bristol <blake+haley@>
2016-03-23 15:01:56 -0700

    move the model to its own file

--- app/app.component.ts
+++ app/app.component.ts
@@ -29,11 +29,3 @@ export class AppComponent {
     console.log(clickedKeg);
   }
 }
-
-//Model////////////////////////////////////
-
-export class Keg {
-  public pintsLeft: number = 124;
-  constructor(public name: string, public brand: string, public price: string, public alContent: string, public id: number) {
-  }
-}
---
+++ app/keg.model.ts
@@ -0,0 +1,7 @@
+//Model////////////////////////////////////
+
+export class Keg {
+  public pintsLeft: number = 124;
+  constructor(public name: string, public brand: string, public price: string, public alContent: string, public id: number) {
+  }
+}
